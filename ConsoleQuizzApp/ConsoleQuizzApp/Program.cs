8e958d84ba414f04dd6a407068834b46439b1af8
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-09-10 13:50:52 +0300

    First questions

--- ConsoleQuizzApp/ConsoleQuizzApp/Program.cs
+++ ConsoleQuizzApp/ConsoleQuizzApp/Program.cs
@@ -4,7 +4,41 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<Question> questions = new List<Question>
+            {
+                new Question
+                {
+                    Text = "This questions answer is A?",
+                    Options = new string[] { "aaa", "bbb", "ccc", "ddd" },
+                    CorrectIndex = 0
+                },
+                new Question
+                {
+                      Text = "This questions answer is C?",
+                    Options = new string[] { "aaa", "bbb", "ccc", "ddd" },
+                    CorrectIndex = 2
+                }
+            };
+
+            foreach (var quest in questions)
+            {
+                string[] optionFormat = new string[] { "   a. ", "   b. ", "   c. ", "   d. "};
+                int answer = 0;
+                Console.WriteLine(quest.Text);
+                for (int i = 0; i < quest.Options.Length; i++)
+                {
+                    Console.WriteLine(optionFormat[i] + quest.Options[i]);
+                }
+                answer = int.Parse(Console.ReadLine());
+                if (quest.IsCorrect(answer))
+                {
+                    Console.WriteLine("Correct"!);
+                }
+                else
+                {
+                    Console.WriteLine("WRONG!");
+                }
+            }
         }
     }
 }
---
+++ ConsoleQuizzApp/ConsoleQuizzApp/Question.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleQuizzApp
+{
+    internal class Question
+    {
+        public string? Text { get; set; }
+        public string[]? Options { get; set; }
+        public int CorrectIndex { get; set; }
+
+        public bool IsCorrect(int answer)
+        {
+            if (answer == CorrectIndex)
+            { return true; }
+            else { return false; }
+        }
+    }
+}
