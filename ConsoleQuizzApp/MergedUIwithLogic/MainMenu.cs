ecfc871c294ca49c997f2c6bda2ece0aec6bb981
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-09-13 15:43:03 +0300

    Improved menu

--- ConsoleQuizzApp/MergedUIwithLogic/MainMenu.cs
+++ ConsoleQuizzApp/MergedUIwithLogic/MainMenu.cs
@@ -13,43 +13,17 @@ namespace MergedUIwithLogic
             Console.OutputEncoding = Encoding.UTF8;
             Console.Clear();
             Console.CursorVisible = false;
-            Console.Write("▓▓");
-            Console.Write(new string(' ', 116));
-            Console.Write("▓▓");
-            Console.WriteLine();
-            Console.Write("▓");
+            MakeFrame();
+            ShowMenuItems();
+        }
 
-            Console.Write(new string(' ', 118));
-            Console.Write("▓");
-            Console.SetCursorPosition(0, 27);
-            Console.WriteLine();
-            Console.Write("▓");
-            Console.Write(new string(' ', 118));
-            Console.Write("▓");
-            Console.WriteLine();
+        private static void ShowMenuItems()
+        {
+            ConsoleKey key;
 
-            Console.Write("▓▓");
-            Console.Write(new string(' ', 116));
-            Console.Write("▓▓");
-
-            ConsoleKey consoleKey;
-            Console.SetCursorPosition(30, 15);
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.Write("Make sure your consoles screen is in the indicated area!");
-            Console.SetCursorPosition(45, 18);
-            Console.ForegroundColor = ConsoleColor.White;
-
-            Console.Write("Press ENTER ⏎ To Continue");
-            do
-            {
-                consoleKey = Console.ReadKey(true).Key;
-                
-            } while (consoleKey != ConsoleKey.Enter);
-            
             int position = 0;
             string[] options = { "Play", "Guide", "Exit" };
 
-            ConsoleKey key;
             do
             {
                 int column = 54;
@@ -84,14 +58,71 @@ namespace MergedUIwithLogic
                 }
 
             } while (key != ConsoleKey.Enter);
-
+            switch (position)
+            {
+                case 0:
+                    break;
+                case 1:
+                    ShowGuide();
+                    break;
+                case 2:
+                    Environment.Exit(0);
+                    break;
+                default:
+                    break;
+            }
             Console.WriteLine($"You selected: {options[position]}");
         }
+
+        private static void ShowGuide()
+        {
+            Console.Clear();
+            Console.WriteLine("You have your jokers on the top left, to use type \"1\", \"2\", \"3\" or \"4\" and press ENTER ⏎");
+            Console.WriteLine("To answer a question press a, b, c or d and ENTER ⏎");
+            Console.WriteLine("To leave early you can press \"e\" and ENTER ⏎, you will earn the amount from the last question");
+            Console.WriteLine("If your answer is not correct, you will recieve the amount from the safety net (if you passed it), safety net questions are colored white");
+            Console.WriteLine("Press ENTER ⏎ to continue");
+            ConsoleKey key;
+            do
+            {
+                key = Console.ReadKey(true).Key;
+            }while (key!=ConsoleKey.Enter );
+            ShowMenuItems();
+        }
+
         public static void MakeFrame()
         {
-            // * 114 *
-            // some rows down
-            // * 114 *
+            ConsoleKey key;
+            Console.SetCursorPosition(30, 15);
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("Make sure your consoles screen is in the indicated area!");
+            Console.SetCursorPosition(45, 18);
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.Write("Press ENTER ⏎ To Continue");
+            do
+            {
+                key = Console.ReadKey(true).Key;
+
+            } while (key != ConsoleKey.Enter);
+            Console.Write("▓▓");
+            Console.Write(new string(' ', 116));
+            Console.Write("▓▓");
+            Console.WriteLine();
+            Console.Write("▓");
+
+            Console.Write(new string(' ', 118));
+            Console.Write("▓");
+            Console.SetCursorPosition(0, 27);
+            Console.WriteLine();
+            Console.Write("▓");
+            Console.Write(new string(' ', 118));
+            Console.Write("▓");
+            Console.WriteLine();
+
+            Console.Write("▓▓");
+            Console.Write(new string(' ', 116));
+            Console.Write("▓▓");
         }
     }
 
